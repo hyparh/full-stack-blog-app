57e8bc3ffba9b8c08fb17617c5788dd88aa85c48
hyparh <hyparh@gmail.com>
2022-11-24 20:13:29 +0200

    Login page stylization ready

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 const Single = () => {
   return (
     <div>Single</div>
-  )
-}
+  );
+};
 
-export default Single
+export default Single;
--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 const Write = () => {
   return (
     <div>Write</div>
-  )
-}
+  );
+};
 
-export default Write
+export default Write;
