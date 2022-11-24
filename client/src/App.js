73edeac4aa179984c2b8e52ca5267e1656ada885
hyparh <hyparh@gmail.com>
2022-11-24 19:21:27 +0200

    Created the main layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,59 @@
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Route, Outlet } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Write from './pages/Write';
+import Home from './pages/Home';
+import Single from './pages/Single';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  )
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>This is HOME!</div>
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/post/:id",
+        element: <Single />
+      },
+      {
+        path: "/write",
+        element: <Write />
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+  {
+    path: "/login",
+    element: <Login />
   },
 ]);
 
 function App() {
   return (
-    <div>
-      <RouterProvider router={router} />
+    <div className='app'>
+      <div className='container'>
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
