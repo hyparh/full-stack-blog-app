3a041f6987c0eb57d8aed79a607013c1b4b5dd4c
hyparh <hyparh@gmail.com>
2022-11-24 16:35:44 +0200

    Initialize main pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>This is HOME!</div>
+  },
+]);
+
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router} />
     </div>
   );
 }
