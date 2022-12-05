9cf3e3f3754b7f5abd2d032962a806eb1fbe5178
hyparh <hyparh@gmail.com>
2022-12-05 11:53:09 +0200

    Side menu visualization ready

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Edit from "../img/edit.png";
 import Delete from "../img/delete.png";
 import { Link } from "react-router-dom";
+import Menu from "../components/Menu";
 
 const Single = () => {
   return (
@@ -31,7 +32,7 @@ const Single = () => {
         <br/>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
       </div>
-      <div className="menu">m</div>
+      <Menu />
     </div>
   );
 };
